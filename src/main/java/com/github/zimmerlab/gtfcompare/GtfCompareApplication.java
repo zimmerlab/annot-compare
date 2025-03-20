6c6517900488e86416aba0ed699bfb101950a0ca
Samuel Klein <it@samuel-klein.net>
2025-03-20 13:34:28 +0100

    project setup

---
+++ src/main/java/com/github/zimmerlab/gtfcompare/GtfCompareApplication.java
@@ -0,0 +1,13 @@
+package com.github.zimmerlab.gtfcompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtfCompareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtfCompareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/zimmerlab/gtfcompare/GtfCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zimmerlab.gtfcompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtfCompareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
