024c8a9efa400dde026eee5eef504e6048592dd2
Samuel Klein <it@samuel-klein.net>
2025-05-22 16:30:06 +0200

    removed vue project and added react

---
+++ annot-compare-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import View from "./components/View.tsx";
+
+function App() {
+  return (
+    <>
+        <div id="jbrowse_linear_genome_view"></div>
+        <View></View>
+    </>
+  )
+}
+
+export default App
---
+++ annot-compare-frontend/src/components/View.tsx
@@ -0,0 +1,102 @@
+import '@fontsource/roboto'
+import {
+    createViewState,
+    JBrowseLinearGenomeView,
+} from '@jbrowse/react-linear-genome-view2'
+
+const assembly = {
+    name: 'GRCh38',
+    sequence: {
+        type: 'ReferenceSequenceTrack',
+        trackId: 'GRCh38-ReferenceSequenceTrack',
+        adapter: {
+            type: 'BgzipFastaAdapter',
+            uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz',
+        },
+    },
+    aliases: ['hg38'],
+    refNameAliases: {
+        adapter: {
+            type: 'RefNameAliasAdapter',
+            uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/hg38_aliases.txt',
+        },
+    },
+}
+
+const tracks = [
+    {
+        type: 'FeatureTrack',
+        trackId: 'ncbi_refseq_109_hg38',
+        name: 'NCBI RefSeq (GFF3Tabix)',
+        assemblyNames: ['GRCh38'],
+        category: ['Annotation'],
+        adapter: {
+            type: 'Gff3TabixAdapter',
+            uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz',
+        },
+    },
+    {
+        type: 'FeatureTrack',
+        trackId: 'ncbi_refseq_109_hg38_2',
+        name: 'NCBI RefSeq (GFF3Tabix)',
+        assemblyNames: ['GRCh38'],
+        category: ['Annotation'],
+        adapter: {
+            type: 'Gff3TabixAdapter',
+            uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz',
+        },
+    },
+]
+
+const defaultSession = {
+    name: 'My session',
+    view: {
+        id: 'linearGenomeView',
+        type: 'LinearGenomeView',
+        tracks: [
+            {
+                type: 'ReferenceSequenceTrack',
+                configuration: 'GRCh38-ReferenceSequenceTrack',
+                displays: [
+                    {
+                        type: 'LinearReferenceSequenceDisplay',
+                        configuration:
+                            'GRCh38-ReferenceSequenceTrack-LinearReferenceSequenceDisplay',
+                    },
+                ],
+            },
+            {
+                type: 'FeatureTrack',
+                configuration: 'ncbi_refseq_109_hg38',
+                displays: [
+                    {
+                        type: 'LinearBasicDisplay',
+                        configuration: 'ncbi_refseq_109_hg38-LinearBasicDisplay',
+                    },
+                ],
+            },
+            {
+                type: 'FeatureTrack',
+                configuration: 'ncbi_refseq_109_hg38_2',
+                displays: [
+                    {
+                        type: 'LinearBasicDisplay',
+                        configuration: 'ncbi_refseq_109_hg38-LinearBasicDisplay',
+                    },
+                ],
+            },
+        ],
+    },
+}
+
+function View() {
+    const state = createViewState({
+        assembly,
+        tracks,
+        location: '10:29,838,737..29,838,819',
+        defaultSession,
+    })
+    return <JBrowseLinearGenomeView viewState={state} />
+}
+
+export default View
