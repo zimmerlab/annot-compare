1376fb4355b65b6bc9f3d1981d732e9aa9bbc22a
meisfabi <mathis.fabian03@gmail.com>
2025-11-09 18:44:02 +0100

    add different with sequence

--- src/main/java/com/github/zimmerlab/gtfcompare/analysis/lifted/LiftedDifferences.java
+++ src/main/java/com/github/zimmerlab/gtfcompare/analysis/lifted/LiftedDifferences.java
@@ -2,7 +2,9 @@ package com.github.zimmerlab.gtfcompare.analysis.lifted;
 
 import com.github.kleinsamuel.gtfutils.GtfFile;
 import com.github.zimmerlab.gtfcompare.model.GenePair;
+import com.github.zimmerlab.gtfcompare.utils.GenomeSequenceExtractor;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,8 +15,9 @@ public class LiftedDifferences {
     private static int normalMappingSize;
     private static int liftedMappingSize;
     private static int numDifferentPairsByPosition;
+    private static int numDifferentPairsBySequence;
 
-    public static void analyze(GtfFile queryGtf, GtfFile targetGtf, GtfFile liftedQueryGtf){
+    public static void analyze(GtfFile queryGtf, GtfFile targetGtf, GtfFile liftedQueryGtf, GenomeSequenceExtractor queryGSE, GenomeSequenceExtractor targetGSE){
         numQueryGenes = queryGtf.getAllGeneFeatureIds().size();
         numTargetGenes = targetGtf.getAllGeneFeatureIds().size();
 
@@ -26,6 +29,8 @@ public class LiftedDifferences {
 
         var differentPairsByPosition = findDifferentPairsByPosition(mappingWithLifted);
         numDifferentPairsByPosition = differentPairsByPosition.size();
+
+        findDifferentPairsBySequence(mappingWithLifted, queryGtf, queryGSE, targetGSE);
     }
 
     private static List<GenePair> find1To1Mapping(GtfFile queryGtf, GtfFile targetGtf){
@@ -54,6 +59,47 @@ public class LiftedDifferences {
 
             if(targetGeneBaseData.getEnd() != queryGeneBaseData.getEnd()) {
                 differentPairs.add(genePair);
+            }
+        }
+
+        return differentPairs;
+    }
+
+    private static List<GenePair> findDifferentPairsBySequence(List<GenePair> mapping, GtfFile queryGtf, GenomeSequenceExtractor queryGSE, GenomeSequenceExtractor targetGSE){
+        var differentPairs = new ArrayList<GenePair>();
+        for(var genePair : mapping){
+            var targetGeneBaseData = genePair.getTargetGene().getBaseData();
+            var queryGeneBaseData = genePair.getQueryGene().getBaseData();
+
+            if(targetGeneBaseData.getStart() != queryGeneBaseData.getStart()) {
+                differentPairs.add(genePair);
+                continue;
+            }
+
+            if(targetGeneBaseData.getEnd() != queryGeneBaseData.getEnd()) {
+                differentPairs.add(genePair);
+                continue;
+            }
+
+            var queryGtfGene = queryGtf.getGeneFeature(genePair.getQueryGene().getGeneId());
+            if(queryGtfGene == null) {
+                differentPairs.add(genePair);
+                System.out.println("wtf");
+                continue;
+            }
+
+            var queryStart = queryGtfGene.getBaseData().getStart();
+            var queryEnd = queryGtfGene.getBaseData().getEnd();
+
+            try {
+                var targetSeq = targetGSE.getSequence(targetGeneBaseData.getContig(), targetGeneBaseData.getStart(), targetGeneBaseData.getEnd());
+                var querySeq = queryGSE.getSequence(queryGtfGene.getBaseData().getContig(), queryStart, queryEnd);
+
+                if(targetSeq.equals(querySeq)) continue;
+
+                differentPairs.add(genePair);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
 
--- src/main/java/com/github/zimmerlab/gtfcompare/runner/LiftedDifferencesRunner.java
+++ src/main/java/com/github/zimmerlab/gtfcompare/runner/LiftedDifferencesRunner.java
@@ -3,6 +3,8 @@ package com.github.zimmerlab.gtfcompare.runner;
 import com.github.kleinsamuel.gtfutils.GtfFile;
 import com.github.zimmerlab.gtfcompare.analysis.clique.CliqueAnalysisGenes;
 import com.github.zimmerlab.gtfcompare.analysis.lifted.LiftedDifferences;
+import com.github.zimmerlab.gtfcompare.parser.FidxParser;
+import com.github.zimmerlab.gtfcompare.utils.GenomeSequenceExtractor;
 import org.apache.commons.cli.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,6 +29,10 @@ public class LiftedDifferencesRunner implements CommandLineRunner {
         o.addOption(Option.builder().longOpt("target-gtf").numberOfArgs(1).required().desc("Path to target gtf file").type(File.class).build());
         o.addOption(Option.builder().longOpt("query-gtf").numberOfArgs(1).required().desc("Path to query gtf file").type(File.class).build());
         o.addOption(Option.builder().longOpt("lifted-query-gtf").numberOfArgs(1).required().desc("Path to lifted query gtf file").type(File.class).build());
+        o.addOption(Option.builder().longOpt("query-fasta").numberOfArgs(1).required().desc("Path to fasta file").type(File.class).build());
+        o.addOption(Option.builder().longOpt("query-fidx").numberOfArgs(1).required().desc("Path to fasta index file").type(File.class).build());
+        o.addOption(Option.builder().longOpt("target-fasta").numberOfArgs(1).required().desc("Path to fasta file").type(File.class).build());
+        o.addOption(Option.builder().longOpt("target-fidx").numberOfArgs(1).required().desc("Path to fasta index file").type(File.class).build());
         o.addOption(Option.builder().longOpt("output").numberOfArgs(1).required().desc("Path to output file").type(File.class).build());
 
         CommandLineParser parser = new DefaultParser();
@@ -60,15 +66,47 @@ public class LiftedDifferencesRunner implements CommandLineRunner {
             System.exit(1);
         }
 
+        if (!cmd.hasOption("target-fasta")) {
+            logger.error("No target fasta specified");
+            System.exit(1);
+        }
+
+        if (!cmd.hasOption("target-fidx")) {
+            logger.error("No target fidx specified");
+            System.exit(1);
+        }
+
+        if (!cmd.hasOption("query-fasta")) {
+            logger.error("No query fasta specified");
+            System.exit(1);
+        }
+
+        if (!cmd.hasOption("query-fidx")) {
+            logger.error("No query fidx specified");
+            System.exit(1);
+        }
+
         var queryPath = cmd.getOptionValue("query-gtf");
         var liftedQueryPath = cmd.getOptionValue("lifted-query-gtf");
         var targetPath = cmd.getOptionValue("target-gtf");
+
+        var targetFasta = cmd.getOptionValue("target-fasta");
+        var targetFidx = cmd.getOptionValue("target-fidx");
+
+        var queryFasta = cmd.getOptionValue("query-fasta");
+        var queryFidx = cmd.getOptionValue("query-fidx");
+
         var outputPath = cmd.getOptionValue("output");
 
         var targetGtf = new GtfFile(new File(targetPath));
         var queryGtf = new GtfFile(new File(queryPath));
         var liftedQueryGtf = new GtfFile(new File(liftedQueryPath));
 
+        var queryFidxMapping = FidxParser.parse(queryFidx);
+        var queryGSE = new GenomeSequenceExtractor(new File(queryFasta), queryFidxMapping);
+
+        var targetFidxMapping = FidxParser.parse(targetFidx);
+        var targetGSE = new GenomeSequenceExtractor(new File(targetFasta),  targetFidxMapping);
         try {
             while (true) {
                 targetGtf.parseNextContig();
@@ -78,9 +116,10 @@ public class LiftedDifferencesRunner implements CommandLineRunner {
                 String t = targetGtf.getParsedContig();
                 String q = queryGtf.getParsedContig();
                 String lq = liftedQueryGtf.getParsedContig();
-                if (!Objects.equals(t, q) ||!Objects.equals(q, lq)) throw new Exception("Contigs do not match Target: " + t + ", Query: " + q + "Lifted Query: " + lq);
+                if (!Objects.equals(t, q) || !Objects.equals(q, lq))
+                    throw new Exception("Contigs do not match Target: " + t + ", Query: " + q + "Lifted Query: " + lq);
 
-                LiftedDifferences.analyze(queryGtf, targetGtf, liftedQueryGtf);
+                LiftedDifferences.analyze(queryGtf, targetGtf, liftedQueryGtf, queryGSE, targetGSE);
             }
         } catch (java.text.ParseException e) {
             logger.debug("Program finished: {}", e.getMessage());
