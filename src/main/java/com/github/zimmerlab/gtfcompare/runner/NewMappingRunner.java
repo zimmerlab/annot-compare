c331b8a017bd55a048cc5477d0e3952472ea2064
Samuel Klein <it@samuel-klein.net>
2026-05-20 10:27:31 +0200

    added support for bgzipped fasta

--- src/main/java/com/github/zimmerlab/gtfcompare/runner/NewMappingRunner.java
+++ src/main/java/com/github/zimmerlab/gtfcompare/runner/NewMappingRunner.java
@@ -1,6 +1,5 @@
 package com.github.zimmerlab.gtfcompare.runner;
 
-
 import com.github.kleinsamuel.gtfutils.GtfConfig;
 import com.github.kleinsamuel.gtfutils.GtfFile;
 import com.github.zimmerlab.gtfcompare.newmapping.Mapping;
@@ -10,13 +9,6 @@ import com.github.zimmerlab.gtfcompare.newmapping.outpututil.MappingOutputWriter
 import com.github.zimmerlab.gtfcompare.newmapping.outpututil.UnmappedWriter;
 import com.github.zimmerlab.gtfcompare.parser.FidxParser;
 import com.github.zimmerlab.gtfcompare.utils.GenomeSequenceExtractor;
-import org.apache.commons.cli.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -25,131 +17,214 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import org.apache.commons.cli.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 @Profile("newMapping")
 @Service
 public class NewMappingRunner implements CommandLineRunner {
-    private final static Logger logger = LogManager.getLogger(NewMappingRunner.class);
+  private static final Logger logger = LogManager.getLogger(NewMappingRunner.class);
 
-    @Override
-    public void run(String... args) throws Exception {
-        var startTime =  System.currentTimeMillis();
-        Options o = new Options();
-        o.addOption(Option.builder().option("h").longOpt("help").desc("Print the help message").build());
-        o.addOption(Option.builder().longOpt("target-gtf").numberOfArgs(1).required().desc("Path to target gtf file").type(File.class).build());
-        o.addOption(Option.builder().longOpt("target-fasta").numberOfArgs(1).required().desc("Path to target fasta file").type(File.class).build());
-        o.addOption(Option.builder().longOpt("query-fasta").numberOfArgs(1).required().desc("Path to query fasta file").type(File.class).build());
-        o.addOption(Option.builder().longOpt("query-fai").numberOfArgs(1).required().desc("Path to query fai file").type(File.class).build());
-        o.addOption(Option.builder().longOpt("target-fai").numberOfArgs(1).required().desc("Path to target fai file").type(File.class).build());
-        o.addOption(Option.builder().longOpt("query-gtf").numberOfArgs(1).required().desc("Path to query gtf file").type(File.class).build());
-        o.addOption(Option.builder().longOpt("output").numberOfArgs(1).required().desc("Path to output file").type(File.class).build());
-        o.addOption(Option.builder().longOpt("allowed-types").hasArg().desc("Comma-separated list of allowed types").build());
-        o.addOption(Option.builder().longOpt("useHomology").numberOfArgs(1).desc("Should Use Seq Homology").type(Boolean.class).build());
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = null;
+  @Override
+  public void run(String... args) throws Exception {
+    var startTime = System.currentTimeMillis();
+    Options o = new Options();
+    o.addOption(
+        Option.builder().option("h").longOpt("help").desc("Print the help message").build());
+    o.addOption(
+        Option.builder()
+            .longOpt("target-gtf")
+            .numberOfArgs(1)
+            .required()
+            .desc("Path to target gtf file")
+            .type(File.class)
+            .build());
+    o.addOption(
+        Option.builder()
+            .longOpt("target-fasta")
+            .numberOfArgs(1)
+            .required()
+            .desc("Path to target fasta file")
+            .type(File.class)
+            .build());
+    o.addOption(
+        Option.builder()
+            .longOpt("query-fasta")
+            .numberOfArgs(1)
+            .required()
+            .desc("Path to query fasta file")
+            .type(File.class)
+            .build());
+    o.addOption(
+        Option.builder()
+            .longOpt("query-fai")
+            .numberOfArgs(1)
+            .required(false)
+            .desc("Path to query fai file")
+            .type(File.class)
+            .build());
+    o.addOption(
+        Option.builder()
+            .longOpt("target-fai")
+            .numberOfArgs(1)
+            .required(false)
+            .desc("Path to target fai file")
+            .type(File.class)
+            .build());
+    o.addOption(
+        Option.builder()
+            .longOpt("query-gtf")
+            .numberOfArgs(1)
+            .required()
+            .desc("Path to query gtf file")
+            .type(File.class)
+            .build());
+    o.addOption(
+        Option.builder()
+            .longOpt("output")
+            .numberOfArgs(1)
+            .required()
+            .desc("Path to output file")
+            .type(File.class)
+            .build());
+    o.addOption(
+        Option.builder()
+            .longOpt("allowed-types")
+            .hasArg()
+            .desc("Comma-separated list of allowed types")
+            .build());
+    o.addOption(
+        Option.builder()
+            .longOpt("useHomology")
+            .numberOfArgs(1)
+            .desc("Should Use Seq Homology")
+            .type(Boolean.class)
+            .build());
+    CommandLineParser parser = new DefaultParser();
+    CommandLine cmd = null;
 
-        try {
-            cmd = parser.parse(o, args);
-        } catch (ParseException e) {
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("newMapping", o, true);
-            System.exit(1);
-        }
-
-        if (!cmd.hasOption("target-gtf")) {
-            logger.error("No target gtf specified");
-            System.exit(1);
-        }
-
-        if (!cmd.hasOption("query-gtf")) {
-            logger.error("No query gtf specified");
-            System.exit(1);
-        }
-
-        if (!cmd.hasOption("output")) {
-            logger.error("No output specified");
-            System.exit(1);
-        }
-
-        Set<String> allowedTypes = null;
-
-        if (cmd.hasOption("allowed-types")) {
-            var set = new HashSet<String>();
-
-            for (var type : cmd.getOptionValue("allowed-types").split(",")) {
-                var defaultType = GtfConfig.getDefault(type);
-                if (defaultType != null) {
-                    set.add(defaultType);
-                } else {
-                    logger.info("Skipping type {} - no default value found", type);
-                }
-            }
-
-            allowedTypes = set.isEmpty() ? null : set;
-        }
-
-        if (cmd.hasOption("allowed-types") && allowedTypes == null) {
-            logger.info("no valid types given - using default values for allowed-types");
-        }
-
-        boolean useSeqHomology = true;
-
-        if(cmd.hasOption("useHomology")){
-            useSeqHomology = Boolean.parseBoolean(cmd.getOptionValue("useHomology"));
-        }
-
-        Similarity.useSeqHomology = useSeqHomology;
-
-        var queryPath = cmd.getOptionValue("query-gtf");
-        var targetPath = cmd.getOptionValue("target-gtf");
-        var targetFastaPath = new File(cmd.getOptionValue("target-fasta"));
-        var queryFastaPath = new File(cmd.getOptionValue("query-fasta"));
-        var targetFaiPath = cmd.getOptionValue("target-fai");
-        var queryFaiPath = cmd.getOptionValue("query-fai");
-        var outputPath = cmd.getOptionValue("output");
-
-        var targetGtf = new GtfFile(new File(targetPath));
-        var queryGtf = new GtfFile(new File(queryPath));
-
-        var targetFai = FidxParser.parse(targetFaiPath);
-        var queryFai = FidxParser.parse(queryFaiPath);
-
-        var targetSequenceExtractor = new GenomeSequenceExtractor(targetFastaPath, targetFai);
-        var querySequenceExtractor = new GenomeSequenceExtractor(queryFastaPath, queryFai);
-
-
-        try (var writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputPath), StandardCharsets.UTF_8));
-             var unmappedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputPath + ".unmapped"), StandardCharsets.UTF_8))) {
-
-            writer.write(MappingFileConstants.OUTPUT_HEADER);
-            unmappedWriter.write("contig\tgeneId\tname\torigin\n");
-            while (true) {
-                targetGtf.parseNextContig();
-                queryGtf.parseNextContig();
-
-                String t = targetGtf.getParsedContig();
-                String q = queryGtf.getParsedContig();
-                if (!Objects.equals(t, q)) throw new Exception("Contigs do not match");
-
-                logger.info("Current Contig: {}", t);
-                var res = Mapping.map(targetGtf, queryGtf, allowedTypes, targetSequenceExtractor, querySequenceExtractor);
-                MappingOutputWriter.write(res.results(), t, writer);
-                UnmappedWriter.write(res.unmappedQueries(), q, "QUERY", unmappedWriter);
-                UnmappedWriter.write(res.unmappedTargets(), t, "TARGET", unmappedWriter);
-            }
-        } catch (java.text.ParseException e) {
-            logger.debug("Program finished: {}", e.getMessage());
-        } catch (Exception e) {
-            logger.error("Program failed", e);
-        }
-
-        var overallTime =  System.currentTimeMillis() - startTime;
-
-        try(var writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputPath + ".overall_time"), StandardCharsets.UTF_8))){
-            writer.write(overallTime + "\n");
-        } catch (Exception e){
-            logger.error("Could not write time", e);
-        }
+    try {
+      cmd = parser.parse(o, args);
+    } catch (ParseException e) {
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp("newMapping", o, true);
+      System.exit(1);
     }
-}
 
+    if (!cmd.hasOption("target-gtf")) {
+      logger.error("No target gtf specified");
+      System.exit(1);
+    }
+
+    if (!cmd.hasOption("query-gtf")) {
+      logger.error("No query gtf specified");
+      System.exit(1);
+    }
+
+    if (!cmd.hasOption("output")) {
+      logger.error("No output specified");
+      System.exit(1);
+    }
+
+    Set<String> allowedTypes = null;
+
+    if (cmd.hasOption("allowed-types")) {
+      var set = new HashSet<String>();
+
+      for (var type : cmd.getOptionValue("allowed-types").split(",")) {
+        var defaultType = GtfConfig.getDefault(type);
+        if (defaultType != null) {
+          set.add(defaultType);
+        } else {
+          logger.info("Skipping type {} - no default value found", type);
+        }
+      }
+
+      allowedTypes = set.isEmpty() ? null : set;
+    }
+
+    if (cmd.hasOption("allowed-types") && allowedTypes == null) {
+      logger.info("no valid types given - using default values for allowed-types");
+    }
+
+    boolean useSeqHomology = true;
+
+    if (cmd.hasOption("useHomology")) {
+      useSeqHomology = Boolean.parseBoolean(cmd.getOptionValue("useHomology"));
+    }
+
+    Similarity.useSeqHomology = useSeqHomology;
+
+    var queryPath = cmd.getOptionValue("query-gtf");
+    var targetPath = cmd.getOptionValue("target-gtf");
+    var targetFastaPath = new File(cmd.getOptionValue("target-fasta"));
+    var queryFastaPath = new File(cmd.getOptionValue("query-fasta"));
+
+    var targetFaiPath = targetFastaPath + ".fai";
+    if (cmd.hasOption("target-fai")) {
+      targetFaiPath = cmd.getOptionValue("target-fai");
+    }
+    var queryFaiPath = queryFastaPath + ".fai";
+    if (cmd.hasOption("query-fai")) {
+      queryFaiPath = cmd.getOptionValue("query-fai");
+    }
+
+    var outputPath = cmd.getOptionValue("output");
+
+    var targetGtf = new GtfFile(new File(targetPath));
+    var queryGtf = new GtfFile(new File(queryPath));
+
+    var targetFai = FidxParser.parse(targetFaiPath);
+    var queryFai = FidxParser.parse(queryFaiPath);
+
+    var targetSequenceExtractor = new GenomeSequenceExtractor(targetFastaPath, targetFai);
+    var querySequenceExtractor = new GenomeSequenceExtractor(queryFastaPath, queryFai);
+
+    try (var writer =
+            new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream(outputPath), StandardCharsets.UTF_8));
+        var unmappedWriter =
+            new BufferedWriter(
+                new OutputStreamWriter(
+                    new FileOutputStream(outputPath + ".unmapped"), StandardCharsets.UTF_8))) {
+
+      writer.write(MappingFileConstants.OUTPUT_HEADER);
+      unmappedWriter.write("contig\tgeneId\tname\torigin\n");
+      while (true) {
+        targetGtf.parseNextContig();
+        queryGtf.parseNextContig();
+
+        String t = targetGtf.getParsedContig();
+        String q = queryGtf.getParsedContig();
+        if (!Objects.equals(t, q)) throw new Exception("Contigs do not match");
+
+        logger.info("Current Contig: {}", t);
+        var res =
+            Mapping.map(
+                targetGtf, queryGtf, allowedTypes, targetSequenceExtractor, querySequenceExtractor);
+        MappingOutputWriter.write(res.results(), t, writer);
+        UnmappedWriter.write(res.unmappedQueries(), q, "QUERY", unmappedWriter);
+        UnmappedWriter.write(res.unmappedTargets(), t, "TARGET", unmappedWriter);
+      }
+    } catch (java.text.ParseException e) {
+      logger.debug("Program finished: {}", e.getMessage());
+    } catch (Exception e) {
+      logger.error("Program failed", e);
+    }
+
+    var overallTime = System.currentTimeMillis() - startTime;
+
+    try (var writer =
+        new BufferedWriter(
+            new OutputStreamWriter(
+                new FileOutputStream(outputPath + ".overall_time"), StandardCharsets.UTF_8))) {
+      writer.write(overallTime + "\n");
+    } catch (Exception e) {
+      logger.error("Could not write time", e);
+    }
+  }
+}
--- src/main/java/com/github/zimmerlab/gtfcompare/utils/GenomeSequenceExtractor.java
+++ src/main/java/com/github/zimmerlab/gtfcompare/utils/GenomeSequenceExtractor.java
@@ -1,25 +1,59 @@
 package com.github.zimmerlab.gtfcompare.utils;
 
 import com.github.zimmerlab.gtfcompare.model.FidxEntry;
+import htsjdk.samtools.util.BlockCompressedInputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.Map;
 
-public class GenomeSequenceExtractor {
-    private RandomAccessFile raf;
-    Map<String, FidxEntry> fidxData;
+public class GenomeSequenceExtractor implements Closeable {
 
     private static final Logger logger = LoggerFactory.getLogger(GenomeSequenceExtractor.class);
 
+    private final RandomAccessFile raf;
+    private final BlockCompressedInputStream bgzfStream;
+    private final boolean isBgzf;
+    private final Map<String, FidxEntry> fidxData;
 
-    public GenomeSequenceExtractor(File fasta, Map<String, FidxEntry> fidxData) throws FileNotFoundException {
-        raf = new RandomAccessFile(fasta, "r");
+    public GenomeSequenceExtractor(File fasta, Map<String, FidxEntry> fidxData) throws IOException {
         this.fidxData = fidxData;
+        this.isBgzf = isBgzfFile(fasta);
+        if (isBgzf) {
+            bgzfStream = new BlockCompressedInputStream(fasta);
+            raf = null;
+            logger.info("Opened {} as BGZF-compressed FASTA", fasta.getName());
+        } else {
+            raf = new RandomAccessFile(fasta, "r");
+            bgzfStream = null;
+        }
+    }
+
+    // Checks for the BGZF magic bytes (gzip + deflate + extra-field flag).
+    // Regular gzip has the same first 2 bytes but lacks the 0x04 extra-field byte,
+    // so this distinguishes BGZF from both plain text and unsupported regular gzip.
+    private static boolean isBgzfFile(File file) throws IOException {
+        try (FileInputStream fis = new FileInputStream(file)) {
+            byte[] magic = new byte[4];
+            int read = fis.read(magic);
+            return read >= 4
+                && (magic[0] & 0xFF) == 0x1f
+                && (magic[1] & 0xFF) == 0x8b
+                && (magic[2] & 0xFF) == 0x08
+                && (magic[3] & 0xFF) == 0x04;
+        }
     }
 
     public String getSequence(String chr, int start, int end) throws IOException {
+        return isBgzf ? getSequenceBgzf(chr, start, end) : getSequenceRaw(chr, start, end);
+    }
+
+    public String fetchExonSequence(String chr, int start, int end) throws IOException {
+        return isBgzf ? fetchExonSequenceBgzf(chr, start, end) : fetchExonSequenceRaw(chr, start, end);
+    }
+
+    private String getSequenceRaw(String chr, int start, int end) throws IOException {
         var fidx = fidxData.get(chr);
         if (fidx == null) throw new IllegalArgumentException("Unknown contig: " + chr);
         if (start < 1 || end < start) throw new IllegalArgumentException("Invalid coordinates");
@@ -28,13 +62,12 @@ public class GenomeSequenceExtractor {
         final int lineLengthWithNewline = fidx.getLineLengthWithNewLine();
         final int newLineLength = lineLengthWithNewline - lineLength;
 
-        final int linesTillStart = (start - 1) / lineLength; // FIX 1
+        final int linesTillStart = (start - 1) / lineLength;
         final long totalLinebreaks = (long) linesTillStart * newLineLength;
-        final long realStart = fidx.getStart() + (start - 1) + totalLinebreaks; // 1-basiert → 0-basiert
+        final long realStart = fidx.getStart() + (start - 1) + totalLinebreaks;
 
         var result = new StringBuilder();
 
-        // Erstes Teilstück bis zum Zeilenende
         raf.seek(realStart);
         int firstChunkMax = Math.min(end - start + 1, lineLength - ((start - 1) % lineLength));
         byte[] buf = new byte[firstChunkMax];
@@ -50,7 +83,7 @@ public class GenomeSequenceExtractor {
         }
 
         int totalLen = end - start + 1;
-        int bytesToRead = totalLen - position; // FIX 2
+        int bytesToRead = totalLen - position;
 
         if (bytesToRead > 0) {
             raf.seek(raf.getFilePointer() + newLineLength);
@@ -73,36 +106,136 @@ public class GenomeSequenceExtractor {
         return result.toString();
     }
 
-    public String fetchExonSequence(String chr, int start, int end) throws IOException {
+    private String fetchExonSequenceRaw(String chr, int start, int end) throws IOException {
         FidxEntry fidx = fidxData.get(chr);
         int lineLen = fidx.getLineLength();
         int linePlusNewline = fidx.getLineLengthWithNewLine();
         int newlineLen = linePlusNewline - lineLen;
 
-        // 1) Zero-based Offset in exon-sequence
-        long zeroBasedStart = start - 1;       // GTF ist 1-based inclusive
+        long zeroBasedStart = start - 1;
         long fullLinesBefore = zeroBasedStart / lineLen;
         long byteOffsetBase = fullLinesBefore * newlineLen + zeroBasedStart;
 
-        // 2) Absolute Dateioffset
-        long filePos = fidx.getStart() + byteOffsetBase;
-        raf.seek(filePos);
+        raf.seek(fidx.getStart() + byteOffsetBase);
 
-        // 3) Jetzt genau (end-start+1) Buchstaben (A/C/G/T/N) lesen, Newlines überspringen
         int toRead = end - start + 1;
         StringBuilder sb = new StringBuilder(toRead);
         while (toRead > 0) {
             int b = raf.read();
-            if (b < 0) break;           // EOF (sollte nicht passieren)
+            if (b < 0) break;
             char c = (char) b;
-            if (c == '\n' || c == '\r') {
-                // skip newline bytes
-                continue;
-            }
+            if (c == '\n' || c == '\r') continue;
             sb.append(c);
             toRead--;
         }
         return sb.toString();
+    }
+
+    // For BGZF, fidx.getStart() is a virtual file pointer (upper 48 bits = compressed block
+    // offset, lower 16 bits = offset within decompressed block). We seek to the contig start
+    // using that virtual pointer, then skip forward through the decompressed bytes to reach
+    // the requested position — arithmetic on decompressed offsets is identical to the raw case.
+    private String getSequenceBgzf(String chr, int start, int end) throws IOException {
+        var fidx = fidxData.get(chr);
+        if (fidx == null) throw new IllegalArgumentException("Unknown contig: " + chr);
+        if (start < 1 || end < start) throw new IllegalArgumentException("Invalid coordinates");
+
+        final int lineLength = fidx.getLineLength();
+        final int lineLengthWithNewline = fidx.getLineLengthWithNewLine();
+        final int newLineLength = lineLengthWithNewline - lineLength;
+
+        final int linesTillStart = (start - 1) / lineLength;
+        final long decompressedOffset = (start - 1) + (long) linesTillStart * newLineLength;
+
+        bgzfStream.seek(fidx.getStart());
+        skipFully(bgzfStream, decompressedOffset);
+
+        var result = new StringBuilder();
+
+        int firstChunkMax = Math.min(end - start + 1, lineLength - ((start - 1) % lineLength));
+        byte[] buf = new byte[firstChunkMax];
+        readFully(bgzfStream, buf);
+
+        int position = 0;
+        for (byte b : buf) {
+            if (b == '\n' || b == '\r') break;
+            position++;
+        }
+        if (position > 0) {
+            result.append(new String(buf, 0, position, java.nio.charset.StandardCharsets.US_ASCII));
+        }
+
+        int totalLen = end - start + 1;
+        int bytesToRead = totalLen - position;
+
+        if (bytesToRead > 0) {
+            skipFully(bgzfStream, newLineLength);
+        }
+
+        int bytesRead = 0;
+        while (bytesRead < bytesToRead) {
+            int currentBytesToRead = Math.min(lineLength, bytesToRead - bytesRead);
+            if (buf.length != currentBytesToRead) buf = new byte[currentBytesToRead];
+            readFully(bgzfStream, buf);
+            result.append(new String(buf, java.nio.charset.StandardCharsets.US_ASCII));
+            bytesRead += currentBytesToRead;
+            if (bytesRead < bytesToRead) {
+                skipFully(bgzfStream, newLineLength);
+            }
+        }
+
+        return result.toString();
+    }
+
+    private String fetchExonSequenceBgzf(String chr, int start, int end) throws IOException {
+        FidxEntry fidx = fidxData.get(chr);
+        int lineLen = fidx.getLineLength();
+        int linePlusNewline = fidx.getLineLengthWithNewLine();
+        int newlineLen = linePlusNewline - lineLen;
+
+        long zeroBasedStart = start - 1;
+        long fullLinesBefore = zeroBasedStart / lineLen;
+        long byteOffsetBase = fullLinesBefore * newlineLen + zeroBasedStart;
+
+        bgzfStream.seek(fidx.getStart());
+        skipFully(bgzfStream, byteOffsetBase);
+
+        int toRead = end - start + 1;
+        StringBuilder sb = new StringBuilder(toRead);
+        while (toRead > 0) {
+            int b = bgzfStream.read();
+            if (b < 0) break;
+            char c = (char) b;
+            if (c == '\n' || c == '\r') continue;
+            sb.append(c);
+            toRead--;
+        }
+        return sb.toString();
+    }
+
+    private static void readFully(InputStream in, byte[] buf) throws IOException {
+        int offset = 0;
+        int remaining = buf.length;
+        while (remaining > 0) {
+            int n = in.read(buf, offset, remaining);
+            if (n < 0) throw new EOFException("Unexpected end of BGZF stream");
+            offset += n;
+            remaining -= n;
+        }
+    }
+
+    private static void skipFully(InputStream in, long n) throws IOException {
+        while (n > 0) {
+            long skipped = in.skip(n);
+            if (skipped <= 0) throw new EOFException("Unexpected end of BGZF stream during skip");
+            n -= skipped;
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (raf != null) raf.close();
+        if (bgzfStream != null) bgzfStream.close();
     }
 
     public static String getReverseComplement(String gene, int start, int end) {
@@ -125,8 +258,6 @@ public class GenomeSequenceExtractor {
                     case 'G' -> 'C';
                     default -> 'N';
                 };
-
-                //reverseComplement[i] = Constants.COMPLEMENT_MAP.get(base);
             }
             return new String(reverseComplement);
         } catch (Exception e) {
